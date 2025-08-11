f4634e7d5707dedea85cf8cae3362f82bfcd8429
Anna_Butalenko <annabutalenko@gmail.com>
2025-08-11 19:31:06 +0300

    correct bug

---
+++ .history/src/provider/TaskProvider_20250811193017.jsx
@@ -0,0 +1,20 @@
+import { TaskContext } from "../context/TaskContext";
+import { useState } from "react";
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved
+      ? JSON.parse(saved)
+      : [{ id: 1, title: "Anna", isDone: false }];
+  });
+
+  const [filterTask, setFilterTask] = useState("all");
+
+  return (
+    <TaskContext.Provider
+      value={{ tasks, setTasks, filterTask, setFilterTask }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
+};
